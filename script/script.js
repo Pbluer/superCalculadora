064e48fa48480199c6f71f7f382aaa8fc6f9c53a
RamonL <46079563+Pbluer@users.noreply.github.com>
2020-09-23 01:10:46 -0300

    0.0.0

---
+++ script/script.js
@@ -0,0 +1,16 @@
+
+var numberOne = document.querySelector('#numberOner')
+var numberTwo = document.querySelector('#numberTwo')
+
+
+
+
+
+submit.addEventListener('submit', makeCalc);
+
+
+function makeCalc(event) {
+    console.log("Oi")
+    event.preventDefault();
+}
+   
